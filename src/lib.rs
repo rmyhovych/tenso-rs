6784e8e0c9d8ba0ba7ee94604bfe196523a3294c
rmyhovych <rmyhovych@gmail.com>
2023-11-25 13:25:23 -0500

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,2 @@
 pub mod matrix;
-pub mod module;
-pub mod operation;
-pub mod optim;
-pub mod activation;
+
---
+++ src/matrix/mod.rs
@@ -0,0 +1,10 @@
+
+
+const PART_WIDTH: usize = 4;
+const PART_SIZE: usize = PART_WIDTH * PART_WIDTH;
+
+struct MatrixPart {
+    data: [f32; PART_SIZE],
+}
+
+pub struct Matrix {}
